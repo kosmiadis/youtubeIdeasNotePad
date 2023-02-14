86a105fc50f5ebb76feb3e6774cbdf4d2d355cec
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2023-02-14 18:39:17 +0200

    Add

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const ideas = document.querySelector('#ideas')
-const addBtn = document.querySelector('.add-idea')
+const addBtn = document.querySelector('#add-idea')
+const cancelBtn = document.querySelector('#cancel')
+const addCont = document.querySelector('.add')
+const addIdeaBtn = document.querySelector('#add')
+const allInputs = document.querySelectorAll('input')
+const textarea = document.querySelector('textarea')
+
 
 addBtn.addEventListener('click', e => {
+    addCont.style.display = 'inline-grid'
+})
+
+cancelBtn.addEventListener('click', e => {
+    addCont.style.display = 'none'
+    allInputs.forEach(inp => inp.value = '')
+    textarea.value = ''
+})
+
+addIdeaBtn.addEventListener('click', e => {
     
 })
-console.log(ideas)
